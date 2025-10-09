b0154ebe703acdcf5984021193a5832d960e6484
Martynas Prascevicius <mp5285j@greenwich.ac.uk>
2025-10-09 17:35:02 +0000

    Merged PR 1:  chore(build): enforce JDK 21 toolchain at root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,36 @@
+// Root build.gradle.kts
+
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+plugins {
+    // keep these at root so their types are available to the script (apply false)
+    kotlin("jvm") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
+    id("org.openjfx.javafxplugin") version "0.1.0" apply false
+}
+
 group = "mpcode"
 version = "0.1.0"
 
 subprojects {
     repositories { mavenCentral() }
 
+    // Java/Scala modules
     plugins.withId("java") {
-        the<org.gradle.api.plugins.JavaPluginExtension>().toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+        // 'extensions.configure' works regardless of the action receiver
+        extensions.configure<JavaPluginExtension> {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(21))
+            }
         }
     }
 
+    // Kotlin modules
     plugins.withId("org.jetbrains.kotlin.jvm") {
-        the<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension>().jvmToolchain(21)
+        extensions.configure<KotlinJvmProjectExtension> {
+            jvmToolchain(21)
+        }
     }
 }
