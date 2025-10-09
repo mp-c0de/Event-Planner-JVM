a9779cc65f5216fc6eda1e7ca16544b859715954
mpcode <mpcode@icloud.com>
2025-10-09 17:46:41 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
-plugins {
-    kotlin("jvm") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
-    id("org.openjfx.javafxplugin") version "0.1.0" apply false
-}
+group = "mpcode"
+version = "0.1.0"
 
-allprojects {
-    group = "mpcode"
-    version = "0.1.0"
+subprojects {
+    repositories { mavenCentral() }
 
-    repositories {
-        mavenCentral()
+    plugins.withId("java") {
+        the<org.gradle.api.plugins.JavaPluginExtension>().toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        the<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension>().jvmToolchain(21)
     }
 }
